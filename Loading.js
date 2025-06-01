132cd24538b421607b609048248866ee1545e07e
DikaFrontend <dikamodelive@gmail.com>
2025-06-01 19:33:22 +0800

    Add files via upload

---
+++ Loading.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.querySelector('.loading-container').style.display = 'none';  // Hide loading after 3 seconds
+    }, 3000);  // Adjust the time here for how long you want the loading to show
+};
